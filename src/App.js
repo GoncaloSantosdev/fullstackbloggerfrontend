c661e6528731da7906920f5ea954b9dd9939fa08
Goncalo <goncalosantos@Goncalos-MacBook-Pro.local>
2022-07-08 11:17:28 -0400

    hw

--- src/App.js
+++ src/App.js
@@ -1,30 +1,66 @@
-import * as React from "react";
-import { useState, useEffect } from "react";
-import "./App.css";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import BlogsPage from "./Pages/Blogs";
+import React from "react";
+
+import "./App.css";
 
 const urlEndpoint = "http://localhost:4000";
 
-const App = () => {
-  const [serverJSON, setServerJSON] = useState({ message: null });
+const App = (props) => {
+  const [serverJSON, setServerJSON] = useState([]);
+  const [sortField, setSortField] = useState("");
+  const [sortOrder, setSortOrder] = useState("ASC");
+  const [filterField, setFilterField] = useState("");
+  const [filterValue, setFilterValue] = useState("");
+  const [limit, setLimit] = useState(10);
+  const [page, setPage] = useState(0);
+
+
+  // Fetch Data from the database
   useEffect(() => {
     const fetchData = async () => {
-      const apiResponse = await fetch(`${urlEndpoint}/blogs/hello-blogs`);
+      const apiResponse = await fetch(`${urlEndpoint}/blogs/all-blogs?sortField=${sortField}&sortOrder=${sortOrder}&filterField=${filterField}&filterValue=${filterValue}&limit=${limit}&page=${page}`);
       const apiJSON = await apiResponse.json();
       setServerJSON(apiJSON);
       return;
     };
+
     fetchData();
-  }, []);
+  }, [sortField, sortOrder, filterField, filterValue, limit, page]);
+
   return (
-    <div className="App">
-      <Routes>
-        <Route
-          path="/blogs"
-          element={<BlogsPage message={serverJSON.message} />}
-        />
-      </Routes>
+    <div className="app">
+      <header className="app-header">
+        <Routes>
+          <Route
+            path="/blogs"
+            element=
+            {<BlogsPage 
+            serverJSON={serverJSON}
+            setServerJSON={setServerJSON}
+
+            sortField={sortField}
+            setSortField={setSortField}
+
+            sortOrder={sortOrder}
+            setSortOrder={setSortOrder}
+
+            filterField={filterField}
+            setFilterField={setFilterField}
+
+            filterValue={filterValue}
+            setFilterValue={setFilterValue}
+
+            limit={limit}
+            setLimit={setLimit}
+
+            page={page}
+            setPage={setPage}
+            />}
+          />
+        </Routes>
+      </header>
     </div>
   );
 };
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,10 +1,72 @@
 import React from "react";
 
-const BlogsPage = ({ message }) => {
+const BlogsPage = (props) => {
   return (
-    <div className="blogs-page">
-      <h1>Blogs Page</h1>
-      <p>Server Message: {message}</p>
+    <div className="section__blogs">
+      <h1 style={{ display: "flex", justifyContent: "center" }}>Blogs Page</h1>
+        <select value={props.sortField} onChange={(e) => {
+            const value = e.target.value;
+            props.setSortField(value);
+          }}
+        >
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+          <option value="createdAt">Generated</option>
+        </select>
+        <select value={props.sortOrder} onChange={(e) => {
+            const value = e.target.value;
+            props.setSortOrder(value);
+          }}
+        >
+          <option value="ASC">Ascend</option>
+          <option value="DESC">Descend</option>
+        </select>
+
+        <h3>Filter Field</h3>
+        <select value={props.filterField} onChange={(e)=> {
+          const value = e.target.value;
+          props.setFilterField(value)
+        }}
+        >
+          <option value="title">Title</option>
+          <option value="author">Author</option>
+        </select>
+
+        <h3>Filter Value</h3>
+        <input type="text" value={props.filterValue} onChange={(e) => {
+            const value = e.target.value;
+            props.setFilterValue(value);
+          }}>
+        </input>
+
+        <h3>Limit</h3>
+        <input type="number" value={props.limit} onChange={(e) => {
+            const value = e.target.value;
+            props.setLimit(value);
+          }}>
+        </input>
+
+        <h3>Page</h3>
+        <input value={props.page} onChange={(e) => {
+            const value = e.target.value;
+            props.setPage(value);
+          }}></input>
+
+          {props.serverJSON.map((blog, index) => {
+          return (
+            <div key={`${blog}-${index}`} style={{ display: "flex",}}>
+              Id: {blog.id}
+              <br></br>
+              Title : {blog.title}
+              <br></br>
+              Author : {blog.author}
+              <br></br>
+              Text: {blog.text}
+              <br></br>
+              {blog.createdAt}
+            </div>
+          );
+        })}
     </div>
   );
 };
